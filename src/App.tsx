58cf0bb6208ccbb04d3bbbe27b343a8de831e363
roniCohen123 <ronicohen3001@gmail.com>
2019-03-24 17:56:43 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,24 @@ import CarOwnerPageContainer from "./components/car-owner-page-container/car-own
 import {SelectValue} from "antd/lib/select";
 import {CarOwnerModel} from "./models/car-owner.model";
 import Login from "./components/login/login";
-
-
 interface Props {
 
 };
 
 interface State {
-    currentCarOwner: CarOwnerModel | undefined
+    currentCarOwner: CarOwnerModel
 };
 
+const carOwners: CarOwnerModel[] = [{Name: "roni cohen", License: "12345678", phoneNumber: "0548130194"},
+    {Name: "inbal galili", License: "5465876", phoneNumber: "0548130194"},
+    {Name: "avi G", License: "3245678", phoneNumber: "0548130194"}];
+
 class App extends React.Component<Props, State> {
-    state: State = {
-        currentCarOwner: undefined
+    onSelect = (value: SelectValue, option: Object): any => {
+        const currentCarOwner = carOwners.find(carOwner => carOwner.License === value);
+        if (currentCarOwner) {
+            this.setState({currentCarOwner});
+        }
     };
 
     render() {
@@ -28,8 +33,10 @@ class App extends React.Component<Props, State> {
         <AppHeader/>
           <HashRouter basename='/'>
               <div className='app-body'>
-                  <Route path='/search' component={SearchCarOwnerContainer}/>
                   <Route path='/login' component={Login}/>
+                  <Route path='/search' render={(props) => (
+                      <SearchCarOwnerContainer carOwners={carOwners} onSelect={this.onSelect}/>
+                  )}/>
               </div>
           </HashRouter>
       </div>
