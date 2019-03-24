bc1edc65dcad33f53d2e1923e374b735d309a9bb
Inbal Gilai <inbal@bringg.com>
2019-03-24 17:53:14 +0200

    login with google

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import GoogleLogin from 'react-google-login';
+import axios from 'axios';
+
+const GOOGLE_TOKEN_INFO_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo";
 
 const Login: React.FunctionComponent = () => {
     const responseGoogle = (response: any) => {
+        const token = response.hg.id_token;
         console.log(response);
+        console.log(token);
+        console.log(window.localStorage);
+
+        axios.get(`${GOOGLE_TOKEN_INFO_URL}?id_token=${token}`).then(response => {
+            const email = response.data.email;
+            console.log(email);
+            window.localStorage.setItem('email', email);
+        });
     };
 
     return (<GoogleLogin
